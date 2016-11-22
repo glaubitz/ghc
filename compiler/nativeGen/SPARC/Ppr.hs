dbaeb58619918a968b258c52abba86031954d0fe
James Clarke <jrtc27@jrtc27.com>
2016-11-22 17:40:30 +0000

    Ppr: hopefully fixed pprInstr point-free definition

--- compiler/nativeGen/SPARC/Ppr.hs
+++ compiler/nativeGen/SPARC/Ppr.hs
@@ -389,7 +389,7 @@ pprDataItem lit
 pprInstr :: Instr -> SDoc
 pprInstr
         = sdocWithPlatform $ \platform ->
-          pprInstr' $ target32Bit platform
+          pprInstr' (target32Bit platform)
 
 
 pprInstr' :: Bool -> Instr -> SDoc
