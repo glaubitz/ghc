695fdd0f173669c3b97bcf145151f01c9479fbd6
James Clarke <jrtc27@jrtc27.com>
2016-11-24 20:52:59 +0000

    Constants: debug hiVersion with trace

--- compiler/main/Constants.hs
+++ compiler/main/Constants.hs
@@ -7,9 +7,13 @@
 module Constants (module Constants) where
 
 import Config
+import Debug.Trace
 
 hiVersion :: Integer
-hiVersion = read (cProjectVersionInt ++ cProjectPatchLevel) :: Integer
+hiVersion =
+    trace ("cProjectVersionInt: " ++ cProjectVersionInt) $
+    trace ("cProjectPatchLevel: " ++ cProjectPatchLevel) $
+    read (cProjectVersionInt ++ cProjectPatchLevel) :: Integer
 
 -- All pretty arbitrary:
 
