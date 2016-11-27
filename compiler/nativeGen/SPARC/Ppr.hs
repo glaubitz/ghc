46d2076a5ea98ccb241933da905290eb76f3927a
James Clarke <jrtc27@jrtc27.com>
2016-11-27 18:26:34 +0000

    Ppr: Missing comma

--- compiler/nativeGen/SPARC/Ppr.hs
+++ compiler/nativeGen/SPARC/Ppr.hs
@@ -644,8 +644,8 @@ pprInstr' is32Bit (BI cond b blockid)
  | b = panic "SPARC.Ppr.pprInstr'.BI: conditional branch with annul bit not yet implemented for V9"
  | otherwise
      = vcat $ map hcat [
-                      [ text "\tb", pprCond cond, char '\t' text "%xcc", comma, text ".+8" ],
-                      [ text "\t b", char '\t', ppr (mkAsmTempLabel (getUnique blockid))   ]
+                      [ text "\tb", pprCond cond, char '\t', text "%xcc", comma, text ".+8" ],
+                      [ text "\t b", char '\t', ppr (mkAsmTempLabel (getUnique blockid))    ]
                   ]
 
 
