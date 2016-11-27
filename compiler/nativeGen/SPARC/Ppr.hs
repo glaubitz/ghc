65cd3b6f5218121e8a6839ab9693a3b641c00b4e
James Clarke <jrtc27@jrtc27.com>
2016-11-27 00:41:41 +0000

    Ppr: needs is32Bit in scope

--- compiler/nativeGen/SPARC/Ppr.hs
+++ compiler/nativeGen/SPARC/Ppr.hs
@@ -606,7 +606,7 @@ pprInstr' _ (FxTOy format1 format2 reg1 reg2)
     ]
 
 
-pprInstr' _ (BI cond b blockid)
+pprInstr' is32Bit (BI cond b blockid)
   = hcat [
         text "\tb", pprCond cond,
         if b then pp_comma_a else empty,
@@ -615,7 +615,7 @@ pprInstr' _ (BI cond b blockid)
         ppr (mkAsmTempLabel (getUnique blockid))
     ]
 
-pprInstr' _ (BF cond b blockid)
+pprInstr' is32Bit (BF cond b blockid)
   = hcat [
         text "\tfb", pprCond cond,
         if b then pp_comma_a else empty,
