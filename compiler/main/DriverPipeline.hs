f660f0a81d06f02280ad31aa30d220b3e6599c85
James Clarke <jrtc27@jrtc27.com>
2016-12-06 18:55:55 +0000

    DriverPipeline: fix syntax?

--- compiler/main/DriverPipeline.hs
+++ compiler/main/DriverPipeline.hs
@@ -285,25 +285,23 @@ compileOne' m_tc_result mHscMessage
 
 compileStub :: HscEnv -> FilePath -> IO FilePath
 compileStub hsc_env0 stub_c = do
-   let
-        hsc_env     = if platformArch (targetPlatform (hsc_dflags hsc_env0)) == ArchSPARC64
-                      then hsc_env_with_gcc_arg hsc_env0 "-mcmodel=medany"
-                      else hsc_env0
+    let hsc_env = if platformArch (targetPlatform (hsc_dflags hsc_env0)) == ArchSPARC64
+                  then hsc_env_with_gcc_arg hsc_env0 "-mcmodel=medany"
+                  else hsc_env0
 
-        (_, stub_o) <- runPipeline StopLn hsc_env (stub_c,Nothing)  Nothing
-                                   Temporary Nothing{-no ModLocation-} Nothing
+    (_, stub_o) <- runPipeline StopLn hsc_env (stub_c,Nothing)  Nothing
+                               Temporary Nothing{-no ModLocation-} Nothing
 
-        return stub_o
-    where
-        hsc_env_with_gcc_arg hsc_env arg =
-            let
-                dflags0   = hsc_dflags hsc_env
-                s0        = settings dflags0
-                (p, args) = sPgm_c s0
-                s1        = s0 { sPgm_c = (p, args ++ [arg]) }
-                dflags1   = dflags0 { settings = s1 }
-            in
-                hsc_env { dflags = dflags1 }
+    return stub_o
+    where hsc_env_with_gcc_arg hsc_env arg =
+        let
+            dflags0   = hsc_dflags hsc_env
+            s0        = settings dflags0
+            (p, args) = sPgm_c s0
+            s1        = s0 { sPgm_c = (p, args ++ [arg]) }
+            dflags1   = dflags0 { settings = s1 }
+        in
+            hsc_env { dflags = dflags1 }
 
 compileEmptyStub :: DynFlags -> HscEnv -> FilePath -> ModLocation -> ModuleName -> IO ()
 compileEmptyStub dflags hsc_env basename location mod_name = do
