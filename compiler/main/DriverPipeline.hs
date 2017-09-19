0d7115109ca2293eaf5dd8d5992d6c8ab35fe007
James Clarke <jrtc27@jrtc27.com>
2017-09-19 09:08:11 +0100

    Revert "mcmodel handled by runPhase; compileForeign no longer needs to"
    This reverts commit 0a844020df592d18ecae4d8b87f3c6c7892ca77b.

--- compiler/main/DriverPipeline.hs
+++ compiler/main/DriverPipeline.hs
@@ -299,20 +299,39 @@ compileOne' m_tc_result mHscMessage
 -- Moreover, we also let the user emit arbitrary C/C++/ObjC/ObjC++ files
 -- from TH, that are then compiled and linked to the module. This is
 -- useful to implement facilities such as inline-c.
+--
+-- On SPARC64, memory allocations happen in high memory, but the default code
+-- model assumes all code is linked in the lower 32 bits of memory. This is not
+-- true if modules are loaded by the RTS, as it uses the high memory
+-- allocations, so we must ensure C stubs get compiled with the medany model
+-- when not using PIC.
+--
 
 compileForeign :: HscEnv -> ForeignSrcLang -> FilePath -> IO FilePath
-compileForeign hsc_env lang stub_c = do
+compileForeign hsc_env0 lang stub_c = do
         let phase = case lang of
               LangC -> Cc
               LangCxx -> Ccxx
               LangObjc -> Cobjc
               LangObjcxx -> Cobjcxx
+            hsc_env = if platformArch (targetPlatform (hsc_dflags hsc_env0)) == ArchSPARC64 &&
+                         not $ gopt Opt_PIC $ hsc_dflags hsc_env0
+                      then hsc_env_with_gcc_arg hsc_env0 $ SysTools.Option "-mcmodel=medany"
+                      else hsc_env0
         (_, stub_o) <- runPipeline StopLn hsc_env
                        (stub_c, Just (RealPhase phase))
                        Nothing (Temporary TFL_GhcSession)
                        Nothing{-no ModLocation-}
                        []
         return stub_o
+        where hsc_env_with_gcc_arg hsc_env arg =
+                  let dflags0   = hsc_dflags hsc_env
+                      s0        = settings dflags0
+                      (p, args) = sPgm_c s0
+                      s1        = s0 { sPgm_c = (p, args ++ [arg]) }
+                      dflags1   = dflags0 { settings = s1 }
+                  in
+                      hsc_env { hsc_dflags = dflags1 }
 
 compileStub :: HscEnv -> FilePath -> IO FilePath
 compileStub hsc_env stub_c = compileForeign hsc_env LangC stub_c
