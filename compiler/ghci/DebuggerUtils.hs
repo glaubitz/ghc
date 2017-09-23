2fb4c1075d30f3e78e5dacf8b10b12dc7a4c1c84
James Clarke <jrtc27@jrtc27.com>
2017-09-23 23:59:44 +0100

    Fix plusPtr syntax

--- compiler/ghci/DebuggerUtils.hs
+++ compiler/ghci/DebuggerUtils.hs
@@ -106,7 +106,7 @@ dataConInfoPtrToName x = do
             | arch == ArchX86_64 || wordSize == 4 = fromIntegral <$> (peek ptr' :: IO Int32)
             | wordSize == 8                       = fromIntegral <$> (peek ptr' :: IO Int64)
             | otherwise                           = panic "getConDescAddress: Unknown word size"
-       return $ (`plusPtr`) (ptr `plusPtr` stdInfoTableSizeB dflags) <$> offsetToStringIO
+       return $ plusPtr (ptr `plusPtr` stdInfoTableSizeB dflags) <$> offsetToStringIO
     | otherwise =
        peek $ intPtrToPtr $ ptrToIntPtr ptr + fromIntegral (stdInfoTableSizeB dflags)
    -- parsing names is a little bit fiddly because we have a string in the form:
